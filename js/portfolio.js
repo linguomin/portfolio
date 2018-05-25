012e3a6c75bc8cba74d126814c9e7e403144bfc4
linguomin <linguomin@outlook.com>
2018-05-25 10:07:00 +0800

    first commit

---
+++ js/portfolio.js
@@ -0,0 +1,31 @@
+$(() => {
+    // /* 作品数据 */
+    // let portfolio = [
+    //     {"title":"足迹-旅游资料网","img":"img/scenery.jpg","href":"#"},
+    //     {"title":"AI科技","img":"img/scenery.jpg","href":"#"},
+    //     {"title":"致敬页","img":"img/scenery.jpg","href":"#"},
+    //     {"title":"贪吃蛇","img":"img/scenery.jpg","href":"#"},
+    //     {"title":"个人博客","img":"img/scenery.jpg","href":"#"},
+    //     {"title":"学子商场后台管理","img":"img/scenery.jpg","href":"#"},
+    // ];
+    // let html = "";
+    // for (let i = 0; i < portfolio.length; i++) {
+    //     html += `
+    //     <div class="col-md-4">
+    //         <div class="card">
+    //             <div class="img">
+    //                 <a href="${portfolio[i].href}">
+    //                     <img class="card-img-top" src="${portfolio[i].img}" alt="Card image">
+    //                 </a>
+    //             </div>
+    //             <div class="card-body">
+    //                 <a href="${portfolio[i].href}">
+    //                     <h4 class="card-title text-center">${portfolio[i].title}</h4>
+    //                 </a>
+    //             </div>
+    //         </div>
+    //     </div>`;
+    // }
+    // $("#portfolio>.row").html(html);
+});
+
